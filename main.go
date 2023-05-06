3e6d0df6a65fd72cddec1caf6f1b99b933a6f087
abdurehman12 <abdurehmanabbasi1243@gmail.com>
2023-05-06 17:57:06 +0500

    After saaf safai

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"archive/zip"
-	"crypto/aes"
-	"crypto/cipher"
+	//"crypto/aes"
+	//"crypto/cipher"
 	"crypto/rand"
+	"encoding/binary"
 	"io"
-	"io/ioutil"
+
+	//"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"project/parallel"
 	"time"
-	"encoding/binary"
 )
 
 // generateKey generates a new random AES key.
@@ -23,140 +25,69 @@ func generateKey() ([]byte, error) {
 	return key, nil
 }
 
-// encryptData encrypts the given data with the given key using AES-256 encryption.
-func encryptData(data []byte, key []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	nonce := make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-	ciphertext := aesgcm.Seal(nil, nonce, data, nil)
-	encryptedData := make([]byte, 0, len(nonce)+len(ciphertext))
-	encryptedData = append(encryptedData, nonce...)
-	encryptedData = append(encryptedData, ciphertext...)
-	return encryptedData, nil
+
+func zipSource2(folderToZip, outputFilePath string) {
+    
+    // Create a new zip archive.
+    zipFile, err := os.Create(outputFilePath)
+    if err != nil {
+        panic(err)
+    }
+    defer zipFile.Close()
+    zipWriter := zip.NewWriter(zipFile)
+    defer zipWriter.Close()
+
+    // Walk through all the directories and files in the folder to zip.
+    err = filepath.Walk(folderToZip, func(filePath string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        
+        // Skip the folder to zip itself.
+        if filePath == folderToZip {
+            return nil
+        }
+        
+        // Create a new zip header for the current file or directory.
+        zipHeader, err := zip.FileInfoHeader(info)
+        if err != nil {
+            return err
+        }
+        zipHeader.Name = filePath[len(folderToZip)+1:]
+
+        // If the current path is a directory, add a trailing slash to the name.
+        if info.IsDir() {
+            zipHeader.Name += "/"
+        }
+
+        // Create a new zip file entry and write the contents to it.
+        zipFileEntry, err := zipWriter.CreateHeader(zipHeader)
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+            fileToZip, err := os.Open(filePath)
+            if err != nil {
+                return err
+            }
+            defer fileToZip.Close()
+            _, err = io.Copy(zipFileEntry, fileToZip)
+            if err != nil {
+                return err
+            }
+        }
+
+        return nil
+    })
+
+    if err != nil {
+        panic(err)
+    }
+    
+	println("Successfully zipped all folders and files!")
+
 }
 
-
-
-// decryptData decrypts the given data with the given key using AES-256 encryption.
-func decryptData(encryptedData []byte, key []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	if len(encryptedData) < aes.BlockSize {
-		return nil, io.ErrUnexpectedEOF
-	}
-	nonce := encryptedData[:12]
-	ciphertext := encryptedData[12:]
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return nil, err
-	}
-	return plaintext, nil
-}
-
-// zip folder to a new zipName.zip
-func zipSource(folderPath, zipName string) {
-	// Create a new zip file and add the contents of the folder to it
-	zipFile, err := os.Create(zipName)
-	if err != nil {
-		panic(err)
-	}
-	defer zipFile.Close()
-
-	archive := zip.NewWriter(zipFile)
-	defer archive.Close()
-
-	files, err := ioutil.ReadDir(folderPath)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, file := range files {
-		if file.IsDir() {
-			continue
-		}
-		fileContent, err := ioutil.ReadFile(folderPath + "/" + file.Name())
-		if err != nil {
-			panic(err)
-		}
-		fileWriter, err := archive.Create(file.Name())
-		if err != nil {
-			panic(err)
-		}
-		_, err = fileWriter.Write(fileContent)
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-// Encrypt zipName.zip to encrypted.zip
-func encryptFile(key []byte, zipName string) {
-	// Read the contents of the zip file and encrypt it
-	data, err := ioutil.ReadFile(zipName)
-	if err != nil {
-		panic(err)
-	}
-	// fmt.Println("size of the zip folder to be encrypted in bytes: ", len(data))
-
-	encryptedData, err := encryptData(data, key)
-	if err != nil {
-		panic(err)
-	}
-
-	// Write the encrypted data to a new file
-	encryptedFile, err := os.Create("encrypted.zip")
-	if err != nil {
-		panic(err)
-	}
-	defer encryptedFile.Close()
-
-	_, err = encryptedFile.Write(encryptedData)
-	if err != nil {
-		panic(err)
-	}
-}
-
-
-// Decrypt encrypted.zip to decrypted.zip
-func decryptFile(key []byte) {
-	// Read the encrypted data from the file and decrypt it
-	encryptedData, err := ioutil.ReadFile("encrypted.zip")
-	if err != nil {
-		panic(err)
-	}
-
-	decryptedData, err := decryptData(encryptedData, key)
-	if err != nil {
-		panic(err)
-	}
-	
-	// Write the decrypted data to a new file
-	decryptedFile, err := os.Create("decrypted.zip")
-	if err != nil {
-		panic(err)
-	}
-	defer decryptedFile.Close()
-
-	_, err = decryptedFile.Write(decryptedData)
-	if err != nil {
-		panic(err)
-	}
-}
 
 
 func main() {
@@ -165,37 +96,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	// key = []byte{164, 251, 124, 160, 192, 76, 167, 99, 197, 62, 83, 81, 98, 17, 42, 11, 209, 147, 211, 24, 160, 49, 82, 62, 163, 137, 218, 18, 113, 122, 57, 100}
-
 	//Set the path of the folder you want to zip and encrypt
-	folderPath := "work2"
+	folderPath := "work"
 
 	//Set the name of the output zip file
 	zipName := "folder.zip"
-
-	//Zip folder to 'zipName.zip'
-	zipSource(folderPath, zipName)
-
-	// start2 := time.Now()
-
-	// // Encrypt 'zipName.zip' to encypted.zip
-	// encryptFile(key, zipName)
-
-	// // Decrypt encypted.zip to decrypted.zip
-	// decryptFile(key)
-	// log.Printf("main, execution time %s\n", time.Since(start2))	
-
+	zipSource2(folderPath, zipName)
 	start := time.Now()
 
 	//Encrypt 'zipName.zip' to encypted.zip
 	parallel.EncryptFileP(key, zipName)
 	
-	// //Decrypt encypted.zip to decrypted.zip
-	// err = parallel.DecryptFileP(key)
-	// if err != nil {
-	// 	panic(err)
-	// }
 	log.Printf("main, execution time %s\n", time.Since(start))
 	log.Println("Done")
 	log.Println(key)
@@ -222,7 +133,7 @@ func main() {
 	// write file size to file
 	err = binary.Write(file, binary.LittleEndian, size)
 	if err != nil {
-		// panic(err)
+		panic(err)
 	}
 
 }
